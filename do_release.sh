36e3376e8d4e5d26f3393f0542b7955f8970e708
hasecilu <hasecilu@tuta.io>
2025-03-07 10:56:04 -0600

    Minor enhancements to release script
    - Simplify conditions (-v flag does the trick, -q for quiet)
    - Escape double parentheses on version example
    - Use double parentheses to expand $VER
    - Avoid "git tag" prompt

--- do_release.sh
+++ do_release.sh
@@ -7,15 +7,15 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
-if [ -n "$(git status --porcelain|grep -v '^??')" ]; then
+if git status --porcelain | grep -q -v '^??'; then
     echo "do_release.sh must be called from a clean working directory."
     exit 2
 fi
 
 VER="$1"
 
-if echo "$VER" | grep -v '^[0-9]\+\.[0-9]\+$'; then
-    echo "do_release.sh must be called with a version number formatted like "1.23" as its first argument, with no leading \"v\"."
+if echo "$VER" | grep -q -v '^[0-9]\+\.[0-9]\+$'; then
+    echo "do_release.sh must be called with a version number formatted like \"1.23\" as its first argument, with no leading \"v\"."
     exit 1
 fi
 
@@ -25,7 +25,6 @@ sed -i "/L\"lolcat version [0-9.]\+, (c) [0-9]\+ jaseg\\\\n\"/s/version [0-9.]\+
 sed -i "s/^pkgver=v[0-9.]\+/pkgver=v$VER/" PKGBUILD
 sed -i "/^AC_INIT/s/\[[0-9.]\+\]/[$VER]/" autotools/configure.ac
 git add lolcat.c PKGBUILD autotools/configure.ac
-git commit -m 'Bump version to v$VER'
-git tag "v$VER"
+git commit -m "Bump version to v$VER"
+git tag "v$VER" -m "Bump version to v$VER"
 echo "Success."
-
